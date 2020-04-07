3e1fe63785153c4a388e79652d68a2e6730249f8
Saurabh <58027263+symbiote-ux@users.noreply.github.com>
2020-04-07 16:55:17 +0530

    modified get_money it return 0 if amount > 31999

--- atm.c
+++ atm.c
@@ -5,6 +5,7 @@ unsigned int get_money(unsigned short int money) {
   unsigned int notes = 0;
   int denominations[] = {1,5,10,20,50,100,500,2000};
   int *ptr = denominations;
+  if(money > 31999) return notes;
   for(int i = 7; i >= 0; i--) {
     notes += pow(16,i) * (money / ptr[i]);
     money = money % ptr[i];
--- main.c
+++ main.c
@@ -3,8 +3,8 @@
 
 int main(void)
 {
-  unsigned short int amounts[] = {1, 10, 39, 589, 2590, 31999};
-  for (int i = 0; i < 6; i++)
+  unsigned short int amounts[] = {1, 10, 39, 589, 2590, 31999,40000};
+  for (int i = 0; i < 7; i++)
   {
     unsigned int notes = get_money(amounts[i]);
     printf("%5u %08x\n", amounts[i], notes);
