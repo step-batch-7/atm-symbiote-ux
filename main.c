f20c81494b55ab0133bd6f4bbdd4b2f35bcade39
Saurabh <58027263+symbiote-ux@users.noreply.github.com>
2020-04-08 08:42:30 +0530

    fixed max limit bug

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 int main(void)
 {
   unsigned short int amounts[] = {1, 10, 39, 589, 2590, 31999};
-  for (int i = 0; i < 7; i++)
+  for (int i = 0; i < 6; i++)
   {
     unsigned int notes = get_money(amounts[i]);
     printf("\nNumber of Notes in Rs %u are: in hex %08x\n", amounts[i], notes);
